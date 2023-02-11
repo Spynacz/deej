0975a86e9188ccbe161b5e67bff537824c0a7d04
Spynacz <63506536+Spynacz@users.noreply.github.com>
2023-02-11 21:37:43 +0100

    Add rudimentary linux deej.current

--- pkg/deej/util/util_linux.go
+++ pkg/deej/util/util_linux.go
@@ -1,9 +1,40 @@
 package util
 
 import (
-	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+const (
+	getCurrentWindowInternalCooldown = time.Millisecond * 350
+)
+
+var (
+	lastGetCurrentWindowResult []string
+	lastGetCurrentWindowCall   = time.Now()
 )
 
 func getCurrentWindowProcessNames() ([]string, error) {
-	return nil, errors.New("Not implemented")
+	// Timeout just like the one in windows util. Might not be necessary, but shouldn't hurt either.
+	now := time.Now()
+	if lastGetCurrentWindowCall.Add(getCurrentWindowInternalCooldown).After(now) {
+		return lastGetCurrentWindowResult, nil
+	}
+
+	lastGetCurrentWindowCall = now
+
+	// Use xprop to get currently active window's PID and use that to output just the process name with ps.
+	// It most likely won't work with sandboxed applications
+	cmd, err := exec.Command("bash", "-c", "ps -q $(xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) _NET_WM_PID | awk '{print $NF}') -o comm=").Output()
+
+	if err != nil {
+		return nil, fmt.Errorf("error getting current process name %w", err)
+	}
+
+	result := []string{strings.TrimSpace(string(cmd))}
+
+	lastGetCurrentWindowResult = result
+	return result, nil
 }
